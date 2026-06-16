e4e241ade8ef3b8939733fe3b627a7ca80a3f625
mintdoll <butakovaarina717@gmail.com>
2026-06-16 21:43:50 +0800

    Add course lesson selection logic

--- EducationalPlatformDesktop/ViewModels/MainViewModel.cs
+++ EducationalPlatformDesktop/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using EducationalPlatformDesktop.Commands;
 using EducationalPlatformDesktop.Mocks;
 using EducationalPlatformDesktop.Models;
@@ -8,11 +9,36 @@ namespace EducationalPlatformDesktop.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private string _pageTitle = "Главная";
-        private string _pageDescription = "Стартовый экран демонстрационной версии приложения.";
-        private string _currentSection = "home";
+        private string _pageTitle = "Мои курсы";
+        private string _pageDescription = "Выберите курс, затем модуль и урок для просмотра лекции.";
 
-        private readonly ObservableCollection<Course> _allCourses = DemoEducationData.GetCourses();
+        private Course? _selectedCourse;
+        private Module? _selectedModule;
+        private Lesson? _selectedLesson;
+        private string _lessonContent = "Выберите урок, чтобы увидеть текст лекции.";
+
+        public MainViewModel()
+        {
+            Courses = DemoEducationData.GetCourses();
+
+            ActiveCoursesCount = Courses.Count(c => c.IsPurchased);
+            LessonsCompletedCount = Courses.Sum(c => c.Modules.Sum(m => m.Lessons.Count)) / 2;
+            CertificatesCount = 1;
+
+            ShowCoursesCommand = new RelayCommand(_ => ShowCourses());
+            ShowCatalogCommand = new RelayCommand(_ => ShowCatalog());
+            ShowTestingCommand = new RelayCommand(_ => ShowTesting());
+            ShowProfileCommand = new RelayCommand(_ => ShowProfile());
+            ExitCommand = new RelayCommand(_ => RequestClose?.Invoke());
+
+            SelectCourseCommand = new RelayCommand(course => SelectCourse(course as Course));
+            SelectModuleCommand = new RelayCommand(module => SelectModule(module as Module));
+            SelectLessonCommand = new RelayCommand(lesson => SelectLesson(lesson as Lesson));
+
+            SelectedCourse = Courses.FirstOrDefault();
+        }
+
+        public event Action? RequestClose;
 
         public string PageTitle
         {
@@ -40,88 +66,195 @@ namespace EducationalPlatformDesktop.ViewModels
             }
         }
 
-        public string CurrentSection
+        public ObservableCollection<Course> Courses { get; }
+
+        public ObservableCollection<Module> Modules
         {
-            get => _currentSection;
+            get;
+            private set;
+        } = new();
+
+        public ObservableCollection<Lesson> Lessons
+        {
+            get;
+            private set;
+        } = new();
+
+        public Course? SelectedCourse
+        {
+            get => _selectedCourse;
             set
             {
-                if (_currentSection != value)
+                if (_selectedCourse != value)
                 {
-                    _currentSection = value;
+                    _selectedCourse = value;
                     OnPropertyChanged();
-                    OnPropertyChanged(nameof(IsHomeVisible));
-                    OnPropertyChanged(nameof(IsCoursesVisible));
-                    OnPropertyChanged(nameof(IsProfileVisible));
+                    UpdateModulesForSelectedCourse();
                 }
             }
         }
 
-        public bool IsHomeVisible => CurrentSection == "home";
-        public bool IsCoursesVisible => CurrentSection == "courses";
-        public bool IsProfileVisible => CurrentSection == "profile";
+        public Module? SelectedModule
+        {
+            get => _selectedModule;
+            set
+            {
+                if (_selectedModule != value)
+                {
+                    _selectedModule = value;
+                    OnPropertyChanged();
+                    UpdateLessonsForSelectedModule();
+                }
+            }
+        }
 
-        public UserProfile Profile { get; } = DemoEducationData.GetProfile();
+        public Lesson? SelectedLesson
+        {
+            get => _selectedLesson;
+            set
+            {
+                if (_selectedLesson != value)
+                {
+                    _selectedLesson = value;
+                    OnPropertyChanged();
+                    UpdateLessonContent();
+                }
+            }
+        }
 
-        public ObservableCollection<Course> AllCourses => _allCourses;
+        public string LessonContent
+        {
+            get => _lessonContent;
+            set
+            {
+                if (_lessonContent != value)
+                {
+                    _lessonContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        public ObservableCollection<Course> PurchasedCourses { get; } = new();
-        public ObservableCollection<Course> AvailableCourses { get; } = new();
+        public int ActiveCoursesCount { get; }
+        public int LessonsCompletedCount { get; }
+        public int CertificatesCount { get; }
 
-        public RelayCommand ShowHomeCommand { get; }
         public RelayCommand ShowCoursesCommand { get; }
+        public RelayCommand ShowCatalogCommand { get; }
+        public RelayCommand ShowTestingCommand { get; }
         public RelayCommand ShowProfileCommand { get; }
         public RelayCommand ExitCommand { get; }
 
-        public event Action? RequestClose;
-
-        public MainViewModel()
-        {
-            ShowHomeCommand = new RelayCommand(_ => ShowHome());
-            ShowCoursesCommand = new RelayCommand(_ => ShowCourses());
-            ShowProfileCommand = new RelayCommand(_ => ShowProfile());
-            ExitCommand = new RelayCommand(_ => RequestClose?.Invoke());
-
-            BuildCourseLists();
-            ShowHome();
-        }
-
-        private void BuildCourseLists()
-        {
-            PurchasedCourses.Clear();
-            AvailableCourses.Clear();
-
-            foreach (var course in AllCourses)
-            {
-                if (course.IsPurchased)
-                {
-                    PurchasedCourses.Add(course);
-                }
-                else
-                {
-                    AvailableCourses.Add(course);
-                }
-            }
-        }
-
-        private void ShowHome()
-        {
-            CurrentSection = "home";
-            PageTitle = "Главная";
-            PageDescription = "Стартовый экран демонстрационного приложения по ТЗ.";
-        }
+        public RelayCommand SelectCourseCommand { get; }
+        public RelayCommand SelectModuleCommand { get; }
+        public RelayCommand SelectLessonCommand { get; }
 
         private void ShowCourses()
         {
-            CurrentSection = "courses";
-            PageTitle = "Курсы";
-            PageDescription = "Список доступных и купленных курсов.";
+            PageTitle = "Мои курсы";
+            PageDescription = "Выберите курс, затем модуль и урок для просмотра лекции.";
+        }
+
+        private void ShowCatalog()
+        {
+            PageTitle = "Каталог";
+            PageDescription = "Список доступных курсов.";
+        }
+
+        private void ShowTesting()
+        {
+            PageTitle = "Тестирование";
+            PageDescription = "Этот раздел будет делать студент 2.";
         }
 
         private void ShowProfile()
         {
-            CurrentSection = "profile";
             PageTitle = "Профиль";
-            PageDescription = "Информация о пользователе.";
+            PageDescription = "Информация об авторизованном пользователе.";
+        }
+
+        private void SelectCourse(Course? course)
+        {
+            if (course == null)
+            {
+                return;
+            }
+
+            SelectedCourse = course;
+            PageTitle = course.Title;
+            PageDescription = $"{course.Track} · {course.Teacher}";
+        }
+
+        private void SelectModule(Module? module)
+        {
+            if (module == null)
+            {
+                return;
+            }
+
+            SelectedModule = module;
+        }
+
+        private void SelectLesson(Lesson? lesson)
+        {
+            if (lesson == null)
+            {
+                return;
+            }
+
+            SelectedLesson = lesson;
+        }
+
+        private void UpdateModulesForSelectedCourse()
+        {
+            Modules.Clear();
+            Lessons.Clear();
+
+            SelectedModule = null;
+            SelectedLesson = null;
+            LessonContent = "Выберите урок, чтобы увидеть текст лекции.";
+
+            if (SelectedCourse == null)
+            {
+                return;
+            }
+
+            foreach (var module in SelectedCourse.Modules)
+            {
+                Modules.Add(module);
+            }
+
+            SelectedModule = Modules.FirstOrDefault();
+        }
+
+        private void UpdateLessonsForSelectedModule()
+        {
+            Lessons.Clear();
+            SelectedLesson = null;
+            LessonContent = "Выберите урок, чтобы увидеть текст лекции.";
+
+            if (SelectedModule == null)
+            {
+                return;
+            }
+
+            foreach (var lesson in SelectedModule.Lessons)
+            {
+                Lessons.Add(lesson);
+            }
+
+            SelectedLesson = Lessons.FirstOrDefault();
+        }
+
+        private void UpdateLessonContent()
+        {
+            if (SelectedLesson == null)
+            {
+                LessonContent = "Выберите урок, чтобы увидеть текст лекции.";
+                return;
+            }
+
+            LessonContent = SelectedLesson.Content;
         }
     }
 }
