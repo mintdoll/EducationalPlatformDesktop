d1cb05a0c96b9a867b8edea36514a7999a8c6a4d
onetwofree123 <elviraovcarenko168@gmail.com>
2026-06-16 20:46:27 +0800

    Add models and mock data

---
+++ EducationalPlatformDesktop/Models/Certificate.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationalPlatformDesktop.Models
+{
+    public class Certificate
+    {
+        public string CourseName { get; set; } = string.Empty;
+        public string StudentName { get; set; } = string.Empty;
+        public string IssuedDate { get; set; } = string.Empty;
+        public int Score { get; set; }
+    }
+}
