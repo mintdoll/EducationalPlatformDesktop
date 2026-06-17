7e2e1b9c2d49d7495858de099db4c5a606b4c428
mintdoll <butakovaarina717@gmail.com>
2026-06-17 08:03:16 +0800

    Текст лекций вынесен в отдельный экран

---
+++ EducationalPlatformDesktop/Views/Sections/LessonView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace EducationalPlatformDesktop.Views.Sections
+{
+    public partial class LessonView : UserControl
+    {
+        public LessonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
